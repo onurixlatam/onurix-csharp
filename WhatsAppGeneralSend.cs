e43ee37e1e488557ed2a075a163e58f6aab4a85a
dev2-apdi <dev2@aplicacionesdinamicas.com>
2025-08-05 10:57:40 -0500

    Refactor WhatsAppSend method to use templateId and remove deprecated files for Shopify and WooCommerce orders

--- WhatsAppGeneralSend.cs
+++ WhatsAppGeneralSend.cs
@@ -14,13 +14,13 @@ namespace PruebaOnurix
         {
             string key = "AQUI_SU_KEY";
             string client = "AQUI_SU_CLIENT";
-            string template = "AQUI_EL_NOMBRE_DE_LA_PLANTILLA";
+            string templateId = "AQUI_EL_ID_DE_LA_PLANTILLA";
             string parameters = "AQUI_EL_JSON_CON_LOS_VALORES_PARA_LA_PLANTILLA"
             using var httpClient = new HttpClient()
             {
                 BaseAddress = new Uri("https://www.onurix.com/"),
             };
-            HttpResponseMessage request = httpClient.PostAsync($"api/v1/whatsapp/send?client={client}&key={key}&template={template}", content: new StringContent(parameters, Encoding.UTF8, "application/json")).Result;
+            HttpResponseMessage request = httpClient.PostAsync($"api/v1/whatsapp/send?client={client}&key={key}&templateId={templateId}", content: new StringContent(parameters, Encoding.UTF8, "application/json")).Result;
             string responseString = request.Content.ReadAsStringAsync().Result;
             Console.WriteLine(responseString);
         }
